6ad7c2367319b1f0016cc362120e667e8e7ed1f1
Leo <Leo@WIN-QM7UTV94LL6>
2023-12-05 01:37:56 -0300

    Adicionar arquivos de projeto.

---
+++ FlowHttp.cs
@@ -0,0 +1,48 @@
+﻿using Flurl.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueflowCS
+{
+    public class FlowHttp
+    {
+        public LockfileContent lockfile;
+        public string Endpoint;
+        public FlurlClient Client;
+        public FlowHttp()
+        {
+            lockfile = new Lockfile().lockfile;
+            Endpoint = $"https://127.0.0.1:{lockfile.Port}";
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain,
+              errors) => true;
+            HttpClient httpClient = new HttpClient(httpClientHandler);
+            httpClient.BaseAddress = new Uri(Endpoint);
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {lockfile.Authorization}");
+            Client = new FlurlClient(httpClient);
+        }
+
+        public async Task<T> GETJsonRequest<T>(string Path, object? Headers = null)
+        {
+            T response = await Client.Request(Path).WithHeaders(Headers).GetJsonAsync<T>();
+            return response;
+        }
+        public async Task<string> GETStringRequest(string Path, object? Headers = null)
+        {
+            string response = await Client.Request(Path).WithHeaders(Headers).GetStringAsync();
+            return response;
+        }
+        public async Task<T> POSTJsonRequest<T>(string Path,  object body, object? Headers = null)
+        {
+            T response = await Client.Request(Path).WithHeaders(Headers).PostJsonAsync(body).ReceiveJson<T>();
+            return response;
+        }
+        public async Task<string> POSTStringRequest(string Path, object body, object? Headers = null)
+        {
+            string response = await Client.Request(Path).WithHeaders(Headers).PostJsonAsync(body).ReceiveString();
+            return response;
+        }
+    }
+}
---
+++ Lockfile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Unicode;
+
+namespace LeagueflowCS
+{
+    public class LockfileContent
+    {
+        public string Authorization { get; set; }
+        public int Port { get; set; }
+    }
+    public class Lockfile
+    {
+        public LockfileContent lockfile;
+
+        public Lockfile()
+        {
+            Process[] ClientProcesses = Process.GetProcessesByName("LeagueClient");
+
+            if (ClientProcesses == null)
+            {
+                Console.WriteLine("LeagueClient.exe not found");
+                return;
+            }
+
+            Process ClientProcess = ClientProcesses[0];
+            string LockfilePath = ClientProcess.MainModule.FileName;
+
+            List<string> ListLockfilePath = LockfilePath.Split('\\').ToList<string>();
+            ListLockfilePath.RemoveAt(ListLockfilePath.Count - 1);
+            ListLockfilePath.Add("lockfile");
+            LockfilePath = string.Join('/', ListLockfilePath);
+
+            using (FileStream LockfileStream = new(LockfilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                using (StreamReader Reader = new(LockfileStream))
+                {
+                    string Content = Reader.ReadToEnd();
+                    string[] SplitContent = Content.Split(':');
+                    lockfile = new LockfileContent
+                    {
+                        Authorization = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"riot:{SplitContent[3]}")),
+                        Port = int.Parse(SplitContent[2])
+                    };
+                }
+            }
+        }
+    }
+}
